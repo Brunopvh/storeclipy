41c1a877ac3079db9d891b3ac5a6e7c81942cfe7
Brunopvh <43321452+Brunopvh@users.noreply.github.com>
2020-08-15 14:47:10 -0400

    Add files via upload

---
+++ lib/os_release.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import os, sys
+import platform
+
+if os.path.isfile('/etc/os-release') == True:
+	release_file = '/etc/os-release'
+elif os.path.isfile('/usr/lib/os-release') == True:
+	release_file = '/usr/lib/os-release'
+elif os.path.isfile('/usr/local/etc/os-release') == True:
+	release_file = '/usr/local/etc/os-release'
+else:
+	print('Arquivo os-release não encontrado')
+
+
+class ReleaseInfo:
+	def __init__(self):
+		self.release_file = release_file
+
+	def get_lines(self):
+		f = open(self.release_file, 'rt')
+		Lines = [] 
+		for L in f.readlines():
+			L = L.replace('\n', '').replace('"', '')
+			Lines.append(L)
+
+		f.close()
+		return Lines
+
+	def get_info(self):
+		lines = self.get_lines()
+		RELEASE_INFO = {'kernel_type': 'Linux'}
+
+		for LINE in lines:
+			if LINE[0:12] == 'PRETTY_NAME=':
+				LINE = LINE.replace('PRETTY_NAME=', '')
+				RELEASE_INFO.update({'PRETTY_NAME': LINE})
+
+			elif LINE[0:5] == 'NAME=':
+				LINE = LINE.replace('NAME=', '')
+				RELEASE_INFO.update({'NAME': LINE})
+
+			elif LINE[0:11] == 'VERSION_ID=':
+				LINE = LINE.replace('VERSION_ID=', '')
+				RELEASE_INFO.update({'VERSION_ID': LINE})
+
+			elif LINE[0:8] == 'VERSION=':
+				LINE = LINE.replace('VERSION=', '')
+				RELEASE_INFO.update({'VERSION': LINE})
+
+			elif LINE[0:17] == 'VERSION_CODENAME=':
+				LINE = LINE.replace('VERSION_CODENAME=', '')
+				RELEASE_INFO.update({'VERSION_CODENAME': LINE})
+
+			elif LINE[0:3] == 'ID=':
+				LINE = LINE.replace('ID=', '')
+				RELEASE_INFO.update({'ID': LINE})
+
+		return RELEASE_INFO
+
+	def show_all(self):
+		RELEASE_INFO = self.get_info()
+		for i in RELEASE_INFO:
+			print(i, '=>', RELEASE_INFO[i])
+
+	def info(self, type_info):
+		RELEASE_INFO = self.get_info()
+
+		if type_info == 'ALL':
+			return RELEASE_INFO
+
+		if type_info in RELEASE_INFO.keys():
+			return str(RELEASE_INFO[type_info]) 
+		else:
+			return 'False'
+
+
+if __name__ == '__main__':
+	#ReleaseInfo().show_all()
+	if (len(sys.argv)) >= int('2'):
+		for info in sys.argv[1:]:
+			i = ReleaseInfo().info(info)
+			print(i)
+	
---
+++ lib/pkg.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+
+import os
+import subprocess
+from time import sleep
+
+columns = os.get_terminal_size(0)[0]
+line = ('-' * columns)
+
+class ProcessLoop:
+	def __init__(self, pid=''):
+		self.pid = pid
+		self.chars = ('-', '\\', '|', '/')
+		self.process_list = []
+
+	def get_process_list(self):
+		'''
+		Obter uma lista com todos os processos em execução no sistema
+		essa lista sera inserida na variável self.process_list
+		'''
+		self.process_list = [] 
+		all_process = subprocess.getstatusoutput('ps aux')[1].split('\n')
+		for i in all_process:
+			self.process_list.append(i) 
+
+		return self.process_list
+
+	def process_loop(self):
+		PROCESS = self.get_process_list()
+		for proc in PROCESS:
+			proc = proc.split()
+			if self.pid in proc[1]:
+				is_pid = 'True'
+				break
+			else:
+				is_pid = 'False'
+
+		if is_pid == 'False':
+			print(f'O processo com PID ({self.pid}) não está em execução.')
+			sleep(0.15)
+			return
+
+		num = int('0')
+		while True:
+			PROCESS = self.get_process_list()
+			for proc in PROCESS:
+				proc = proc.split()
+				if self.pid in proc[1]:
+					is_pid = 'True'
+					break
+				else:
+					is_pid = 'False'
+
+			if is_pid == 'False':
+				print(f'Aguardando processo com pid ({self.pid}) \033[0;33mfinalizado\033[m [{self.chars[num]}]')
+				sleep(0.1)
+				break
+				return
+			else:
+				print(f'\033[KAguardando processo com pid ({self.pid}) finalizar [{self.chars[num]}]', end='\r')
+				sleep(0.15)
+
+			if num == int('3'):
+				num = int('-1')
+			num += 1
+			
+
+class Pkg:
+
+	def __init__(self):
+		pass
+			
+	def pkg_is_list(self, pkgs):
+		'''
+		Verificar se os pacotes foram passados para classe em forma de lista
+		'''
+		if isinstance(pkgs, list): 
+			return 'True'
+		else:
+			print('\033[0;31mFalha o(s) pacotes para instalação precisam ser passados em forma de uma lista.\033[m') 
+			print(__class__)
+			return 'False'
+	
+	def pkg_process_loop(self):
+		all_procs = ' '
+		
+		while True:
+			all_procs = ProcessLoop().get_process_list()
+			for P in all_procs:
+				if ('pkg install' in P) or ('pkg update' in P) or ('pkg delete' in P):
+					pid_pkg = P.split()[1] # Converter a linha de saída em lista e retornar o segundo item.
+					sleep(0.1)
+					break
+				else:
+					pid_pkg = None
+
+			if pid_pkg == None:
+				break
+			else:				
+				ProcessLoop(pid_pkg).process_loop()
+				
+	def install(self, pkgs):
+		if self.pkg_is_list(pkgs) == 'False':
+			return
+
+		self.pkg_process_loop() # Verificar se existe outro processo 'apt' em execução no sistema.
+
+		for APP in pkgs: # Instalar cada pacote da lista, individualmente.
+			print(line)
+			print(f'Instalando: {APP}')			
+			print(line)
+			os.system(f'sudo pkg install {APP}')
+
+	def update(self):
+		print(line)
+		self.pkg_process_loop()
+		print('Executando: sudo pkg update')
+		os.system('sudo pkg update')
+
+if __name__ == '__main__':
+	AptGet().install(sys.argv[1:])
+	
+
+
+   
+        
+
